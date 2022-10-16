bb96e89f81e9517c306e94e9cabcc5ccdde49248
erigra <erikgrande71@gmail.com>
2022-10-16 14:31:27 +0200

    t

--- code/main.py
+++ code/main.py
@@ -1,11 +1,20 @@
 import pygame, sys 
 from settings import *
+from player import Player
 
 # basic setup
 pygame.init()
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Frogger')
 clock = pygame.time.Clock()
+
+# Groups
+all_sprites = pygame.sprite.Group()
+
+# create player
+
+player = Player((600,400), all_sprites)
+
 
 # game loop
 while True:
@@ -18,6 +27,9 @@ while True:
 
 	# delta time 
 	dt = clock.tick() / 1000
+
+	# draw
+	all_sprites.draw(display_surface)
 
 	# update the display surface -> drawing the frame 
 	pygame.display.update()
